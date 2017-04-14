2b02986e54d82b41b1f649b7b6e514dea811be0a
MarinaMarch <Mari.rubel@gmail.com>
2017-04-14 15:26:55 +0300

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+import math
+import numpy
+
+x = int(input("Enter the number x: "))
+y = int(input("Enter the number y: "))
+print ("x**y =",x ** y)
+print ("log(x) =", numpy.log2(x))
+
+
